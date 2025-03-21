2f0e808c138de313e16881240b41151cbdf10206
Mull1gan <russellproductiongroup@gmail.com>
2025-03-21 12:42:57 -0400

    Thirds Commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -152,7 +152,7 @@ const Hero = () => {
             {/* Image */}
             <div className="absolute inset-2 rounded-full overflow-hidden border-2 border-blue-500/50">
               <Image
-                src="/Mulligan.jpg"
+                src="/me.svg"
                 alt="John Russell"
                 fill
                 priority
